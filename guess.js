b1b2e09f1fbd1e62f8c46db54e9369f42a3efd77
Cristina <cristina.colon+git@gmail.com>
2015-07-13 10:32:28 -0400

    refactoring and adding comments throughout

--- guess.js
+++ guess.js
@@ -1,20 +1,34 @@
 var target;
 var maxGuesses = 5;
 var guesses = [];
+/*
+ * guessesRemaining serves as shorthand to reaching this value that 
+ * we'll need several times in the code, yet would be redundant to 
+ * store and track separately. 
+ */
 var guessesRemaining = function () {return maxGuesses-guesses.length;};
 
 $(document).ready(function(){
 	target =  setTarget();
+	/* 
+	 * User can submit guess by clicking a button or hitting enter.
+	 * Input field automatically cleared on enter and on focus event.
+	 */
 	$('#guessButton').on('click', checkGuess);
 	$('#guessBox').on('keyup', function(e){
 		if (e.which==13) {checkGuess();$(this).val('');}
 	})
+	$('#guessBox').on('focus', function () {$(this).val('');});
 	$('#startOver').on('click', startOver);
 	$('#getHint').on('click', getHint);
-	$('#guessBox').on('focus', function () {$(this).val('');});
 })
 
 function validateInput(){
+	/*
+	 * Accepts and returns the input if and only if there are guesses
+	 * remaining, the game hasn't been won yet, and the input is a number
+	 * between 1 and 100 (inclusive) that hasn't been guessed already.
+	 */
 	var input = +$('#guessBox').val();
 	var msg = "";
 	if ( guesses.indexOf(target)>-1) {
@@ -43,82 +57,120 @@ function validateInput(){
 }
 
 function checkGuess(){
+	/* 
+	 * currGuess will take on the value of the input if it
+	 * was found to be valid, or false if not.
+	 */
 	var currGuess = validateInput();
-	var msg = "";
+	/*
+	 * If the input value was passed to it above, currGuess 
+	 * will read as true in the conditional.
+	 */
 	if (currGuess) {
+		var msg = "";
+		/* 
+		 * If valid input, push it to the guesses array. Then take
+		 * action based on how the guess compares to the target.
+		 */
+		guesses.push(currGuess)
 		if (currGuess==target) {
-			guesses.push(currGuess)
-			msg = 'You got it! The answer is '+target+"!";
-			$('#msg').text(msg);
-			/* Voila my creative flourish */
-			$("#list").fadeOut('slow');
-			$("#guessCounter").fadeOut('slow');
+			msg = 'You got it! The answer is '+target+'!';
+			/* 
+			 * Voilà my creative flourish!
+			 * fadeTo() preferred over fadeOut() because we
+			 * want the elements to continue taking up space. 
+			 */
+			$("#list").fadeTo(1000,0);
+			$("#guessCounter").fadeTo(1000,0);
 		}
 		else {
-			guesses.push(currGuess)
-			
-			var status = "";
-			if (Math.abs(currGuess-target)<10) {status = "hot";}
-			else {status="cold";}
-
-			msg+= "You are "+status+". "; 
-
-			if (guesses.length>1) {
-				var progress = "";
-				/* 
-				 * Because the current guess has already been pushed to the array, its index is
-				 * guesses.length-1, and the index of the guess before it is guesses.length-2.
-				 */
-				var prevGuess = guesses[guesses.length-2];
-				if (Math.abs(currGuess-target)<Math.abs(prevGuess-target)) {progress="closer";}
-				else {progress="further away";}
-				msg+="That was "+progress+" than your last guess. "
-			}
-
-			var direction = "";
-			if (currGuess<target) {direction="higher";}
-			else {direction="lower";}
-
-			msg+= "Guess "+direction+"!"
+			/* 
+			 * If the guess was not on target, add to the visual list
+			 * and continue the game.
+			 */
 			updateList();
 
-			if ( guessesRemaining() < 1){ msg = 'Sorry, you\'re out of guesses.';}
-			$('#msg').text(msg);
+			/*
+			 * If this was the user's last chance to get it right, 
+			 * give the game over message.
+			 */
+			if ( guessesRemaining() < 1){ 
+				msg = 'Sorry, you\'re out of guesses.';
+			}
+			else {
+				/*
+				 * If the guess was not on target and the game is 
+				 * not over, compose a message based on how close 
+				 * the guess was (var status), whether it was better
+				 * or worse than the last guess (var progress), and 
+				 * whether the user should guess higher or lower 
+				 * (var direction). 
+				 */
+				var msg = "";
+				var status = "";
+				if (Math.abs(currGuess-target)<10) {status = "hot";}
+				else {status="cold";}
+
+				msg+= "You are "+status+". "; 
+
+				/*
+				 * Only attempt comparison to previous entries if 
+				 * guesses array is holding more than one guess in it.
+				 */
+				if (guesses.length>1) {
+					var progress = "";
+					/* 
+					 * Since the current guess has already been pushed to the array, its index is
+					 * guesses.length-1, and the index of the guess before it is guesses.length-2.
+					 */
+					var prevGuess = guesses[guesses.length-2];
+					if (Math.abs(currGuess-target)<Math.abs(prevGuess-target)) {progress="closer";}
+					else {progress="further away";}
+					msg+="That was "+progress+" than your last guess. "
+				}
+
+				var direction = "";
+				if (currGuess<target) {direction="higher";}
+				else {direction="lower";}
+
+				msg+= "Guess "+direction+"!"
+			}
+			
 		}
+		/* Display the appropriate message */
+		$('#msg').text(msg);
 	}
 }
 
 function updateList(){
-
 	if (guessesRemaining() < 1) {
 		$('#guessCounter').text('Click "Start over" to do it again!');
 	}
 	else {
 		$("#guessesRemaining").text(guessesRemaining());
 	}
-
 	/* 
 	 * Borrow the values from guesses array, because we're
 	 * going to make modifications before displaying.
 	 */
 	var visualList = guesses.map(function(a){return a;});
 	/* 
-	 * Add the target value, sort the array numerically, then
-	 * find its position in the array.
+	 * Add the target value, then sort the array numerically.
 	 */
 	visualList.push(target)
 	visualList.sort(function(a,b){return a-b;})
 
-	/*
-	 * Transform array elements jQuery objects too.
-	 */
+	/* Add HTML markup to each array object accordingly. */
 	for (var i=0;i<visualList.length;i++) {
 		var value = visualList[i];
-
+		/* Replace the value with a question mark */
 		if (value==target) {
 			visualList[i] = '<span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>';
 		}
-
+		/* 
+		 * Add colors to previous guesses based on where they are
+		 * in relation to the target.
+		 */
 		else {
 			var classes = "";
 
@@ -136,25 +188,35 @@ function updateList(){
 }
 
 function startOver() {
+	/* Sets new target */
 	target = setTarget();
-	/* One method of clearing an array is to set its length to 0. */
-	guesses.length = 0 
-	$('#list').text("");
-	var msg = "Your game has been restarted, submit a new guess!"
-	$('#guessCounter').html("<span id='guessesRemaining'>5</span> guesses remaining")
-	$('#msg').text(msg);
+	/* Clears guesses array. One method to do that is to set its length to 0. */
+	guesses.length = 0;
+	/* Clear the list display and the input field. */ 
+	$('#list').text('');
 	$("#guessBox").val('');
+	/* Refresh contents of guessCounter and msg. */
+	$('#guessCounter').html("<span id='guessesRemaining'>5</span> guesses remaining")
+	$('#msg').text("Your game has been restarted, submit a new guess!");
 	/* 
-	 * We call show #list and #guessCounter again because they will have been
-	 * hidden if the game has been won.
+	 * Call show() on #list and #guessCounter because they 
+	 * will have been hidden if the game has been won.
 	 */
 	$('#list').show();
 	$('#guessCounter').show();
+	/* 
+	 * Sets Start Over button color back to normal because 
+	 * it could have been turned green earlier.
+	 */
 	$("#startOver").removeClass("btn-success");
 	$("#startOver").addClass("btn-default");
 }
 
 function getHint() {
+	/* 
+	 * Gives away the answer, per spec. Requires user to have made 
+	 * at least one guess beforehand.
+	 */
 	if (guesses.length>0){
 		$('#guessCounter').text(target);
 	}
